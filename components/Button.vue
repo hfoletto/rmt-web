1b29e99969173c3ce61f5299f1aa5d33d9e24a72
Henrique Foletto <henriquevidalfoletto@gmail.com>
2022-11-26 18:35:34 -0300

    add <Button> component

---
+++ components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+  <button type="submit" class="flex w-full justify-center rounded-md border border-transparent bg-red-800 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-offset-2">
+    <slot/>
+  </button>
+</template>
