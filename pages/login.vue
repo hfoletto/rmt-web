19987f5fc8e0906037f024a4c0d65a502f925886
Henrique Foletto <henriquevidalfoletto@gmail.com>
2022-11-26 12:12:09 -0300

    Add login page

---
+++ pages/login.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="flex min-h-screen">
+    <div class="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div class="mx-auto w-full max-w-sm lg:w-96">
+        <div>
+          <img class="h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+          <h2 class="mt-6 text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+          <p class="mt-2 text-sm text-gray-600">
+            Or
+            {{ ' ' }}
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+          </p>
+        </div>
+
+        <div class="mt-8">
+          <div class="mt-6">
+            <form action="#" method="POST" class="space-y-6" @submit.prevent="login">
+              <div>
+                <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
+                <div class="mt-1">
+                  <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autocomplete="email"
+                      required=""
+                      class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      v-model="email"
+                  />
+                </div>
+              </div>
+
+              <div class="space-y-1">
+                <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                <div class="mt-1">
+                  <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autocomplete="current-password"
+                      required=""
+                      class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      v-model="password"
+                  />
+                </div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="flex items-center">
+                  <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                  <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+                </div>
+
+                <div class="text-sm">
+                  <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+                </div>
+              </div>
+
+              <div>
+                <button type="submit" class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Sign in</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="relative hidden w-0 flex-1 lg:block">
+      <img class="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80" alt="" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useHead } from "nuxt/app";
+
+const title = ref('My App')
+const description = ref('My App Description')
+// This will be reactive even you change title/description above
+useHead({
+  title,
+  meta: [{
+    name: 'description',
+    content: description
+  }],
+  htmlAttrs: {
+    class: 'h-full bg-white'
+  },
+  bodyAttrs: {
+    class: 'h-full'
+  }
+})
+
+const email = ref("")
+const password = ref("")
+
+async function login() {
+  const variables = { email: email.value, password: password.value }
+  useAsyncQuery(loginMutation, variables)
+      .then(result => {
+        console.log(result)
+      })
+      .catch(e => {
+        console.log("DEU RUIM")
+        console.log(e)
+      })
+}
+
+const loginMutation = gql`
+  mutation login($email: String!, $password: String!) {
+  login (email: $email, password: $password) {
+    name,
+    email
+  }
+}
+`
+
+const theatersQuery = gql`
+  query getTheaters {
+  theaters (first: 10) {
+    data {
+      name
+    }
+  }
+}
+`
+</script>
