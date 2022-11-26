b0472dc42d295ac45253bb467d801ecc5ab51945
Henrique Foletto <henriquevidalfoletto@gmail.com>
2022-11-26 12:09:38 -0300

    Update nuxt.config.ts to use the required modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+import { defineNuxtConfig } from 'nuxt/config'
 
+export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/apollo',
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+    ],
+    head: {
+        link: [
+            {
+                rel: "stylesheet",
+                href: "https://rsms.me/inter/inter.css",
+            },
+        ],
+    },
+    ssr: false,
+    apollo: {
+        clients: {
+            default: {
+                httpEndpoint: 'http://localhost/graphql',
+                httpLinkOptions: {
+                    credentials: 'include'
+                }
+            }
+        },
+    },
 })
