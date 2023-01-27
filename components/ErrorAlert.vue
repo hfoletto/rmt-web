a52c6243e8fc8466db031b476982702d597845ae
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-01-27 01:58:17 -0300

    Add ErrorAlert component

---
+++ components/ErrorAlert.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="space-y-2">
+    <Alert theme="error" v-for="(error, i) of error.graphQLErrors" :title="error.message">
+      <ul v-if="error.extensions.validation" role="list" class="list-disc space-y-1 pl-5">
+        <li v-for="(value, key) of error.extensions.validation" :key="key">
+          {{ value.join(', ') }}
+        </li>
+      </ul>
+    </Alert>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from "@vue/runtime-core";
+import { ApolloError } from "@apollo/client";
+const props = defineProps({
+  error: Object as PropType<ApolloError>,
+})
+</script>
