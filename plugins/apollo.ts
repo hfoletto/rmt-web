755e93371aefaee0f17e96fc2c1cb02bcd481e46
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-01-27 01:57:25 -0300

    Remove error handling from apollo.ts plugin file

--- plugins/apollo.ts
+++ plugins/apollo.ts
@@ -1,19 +1,10 @@
-import {ErrorResponse} from "@nuxtjs/apollo";
-import {GraphQLError} from "graphql/index";
-
 export default defineNuxtPlugin((nuxtApp) => {
     // access cookie for auth
     const cookie = useCookie('XSRF-TOKEN')
 
-    nuxtApp.hook('apollo:error', (error: ErrorResponse) => {
-        error.graphQLErrors?.forEach((graphQLError: GraphQLError) => {
-            console.log(graphQLError.debugMessage)
-        })
-    })
-
     nuxtApp.hook('apollo:auth', ({ client, token }) => {
-        console.log(client)
-        console.log(token)
+        // console.log(client)
+        // console.log(token)
         // `client` can be used to differentiate logic on a per client basis.
         // apply apollo client token
         // token.value = '<secret_token>'
