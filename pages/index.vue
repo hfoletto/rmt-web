f32b7c7f4fa12315b2ff0484dcab3510686fbdb2
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-01-27 02:00:39 -0300

    add index page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>{{ JSON.stringify(data.me.email) }}</h2>
+</template>
+
+<script lang="ts" setup>
+const query = gql`
+  query getMe {
+  me {
+    email,
+    name,
+    created_at
+  }
+}
+`
+const { data } = await useAsyncQuery(query)
+</script>
