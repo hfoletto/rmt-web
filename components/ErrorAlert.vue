dbf2451542cf79f16c7c0056cb2e5de8295a71c0
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-01-28 00:40:32 -0300

    Update ErrorAlert to use defineProps with generic type

--- components/ErrorAlert.vue
+++ components/ErrorAlert.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="space-y-2">
-    <Alert theme="error" v-for="(error, i) of error.graphQLErrors" :title="error.message">
+    <Alert theme="error" v-for="(error) of error.graphQLErrors" :title="error.message">
       <ul v-if="error.extensions.validation" role="list" class="list-disc space-y-1 pl-5">
         <li v-for="(value, key) of error.extensions.validation" :key="key">
           {{ value.join(', ') }}
@@ -11,9 +11,10 @@
 </template>
 
 <script setup lang="ts">
-import { PropType } from "@vue/runtime-core";
-import { ApolloError } from "@apollo/client";
-const props = defineProps({
-  error: Object as PropType<ApolloError>,
-})
+import { ApolloError } from '@apollo/client'
+const props = defineProps<{
+  error: ApolloError
+}>()
 </script>
+
+
