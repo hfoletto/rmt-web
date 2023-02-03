f3f5a9b83e3feddce9c93c15fa6cce86fdfd814c
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-02-03 00:15:13 -0300

    Add individual ratings in cinemas index page

--- pages/cinemas/index.vue
+++ pages/cinemas/index.vue
@@ -9,20 +9,45 @@
           </h2>
         </div>
         <div class="mt-8 grid gap-12 pt-8 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-4">
-          <div v-for="theater in city.theaters" :key="theater.name" class="bg-white flex flex-col overflow-hidden rounded-lg shadow-lg p-5">
+          <div v-for="theater in city.theaters" :key="theater.name" class="bg-white flex flex-col rounded-lg shadow-lg p-5">
             <a href="#" class="block">
               <p class="text-xl font-semibold text-gray-900">{{ theater.name }}</p>
               <span class="text-base text-gray-500">{{ theater.address }}</span>
             </a>
             <div class="mt-6 space-y-4">
               <div v-for="auditorium in theater.auditoriums" :key="auditorium.name">
-                <h4>{{ auditorium.name }}</h4>
+                <h4>{{ auditorium.name }} - {{ auditorium.ratings_count }} avaliações</h4>
                 <div class="flex flex-wrap items-center space-x-1.5">
-                  <RatingChip type="image" :value="auditorium.image_rating"/>
-                  <RatingChip type="audio" :value="auditorium.audio_rating"/>
-                  <RatingChip type="comfort" :value="auditorium.comfort_rating"/>
-                  <RatingChip type="bomboniere" :value="auditorium.bomboniere_rating"/>
-                  <RatingChip type="experience" :value="auditorium.experience_rating"/>
+                  <div class="group relative">
+                    <RatingChip data-tooltip-target="image_rating_tooltip" type="image" :value="auditorium.image_rating"/>
+                    <span class="px-3 py-2 text-sm font-medium bg-gray-700 text-white rounded-lg shadow-sm whitespace-nowrap -translate-y-full -translate-x-1/2 -top-1 left-2/4 hidden group-hover:block absolute z-50">
+                      Imagem - {{ auditorium.image_rating_count }} avaliações
+                    </span>
+                  </div>
+                  <div class="group relative">
+                    <RatingChip type="audio" :value="auditorium.audio_rating"/>
+                    <span class="px-3 py-2 text-sm font-medium bg-gray-700 text-white rounded-lg shadow-sm whitespace-nowrap -translate-y-full -translate-x-1/2 -top-1 left-2/4 hidden group-hover:block absolute z-50">
+                      Áudio - {{ auditorium.audio_rating_count }} avaliações
+                    </span>
+                  </div>
+                  <div class="group relative">
+                    <RatingChip type="comfort" :value="auditorium.comfort_rating"/>
+                    <span class="px-3 py-2 text-sm font-medium bg-gray-700 text-white rounded-lg shadow-sm whitespace-nowrap -translate-y-full -translate-x-1/2 -top-1 left-2/4 hidden group-hover:block absolute z-50">
+                      Conforto - {{ auditorium.comfort_rating_count }} avaliações
+                    </span>
+                  </div>
+                  <div class="group relative">
+                    <RatingChip type="bomboniere" :value="auditorium.bomboniere_rating"/>
+                    <span class="px-3 py-2 text-sm font-medium bg-gray-700 text-white rounded-lg shadow-sm whitespace-nowrap -translate-y-full -translate-x-1/2 -top-1 left-2/4 hidden group-hover:block absolute z-50">
+                      Bomboniere - {{ auditorium.bomboniere_rating_count }} avaliações
+                    </span>
+                  </div>
+                  <div class="group relative">
+                    <RatingChip type="experience" :value="auditorium.experience_rating"/>
+                    <span class="px-3 py-2 text-sm font-medium bg-gray-700 text-white rounded-lg shadow-sm whitespace-nowrap -translate-y-full -translate-x-1/2 -top-1 left-2/4 hidden group-hover:block absolute z-50">
+                      Experiência - {{ auditorium.experience_rating_count }} avaliações
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
@@ -68,14 +93,20 @@ const query = gql`
         }
         theaters {
           name
+          address
           auditoriums_count
           auditoriums {
             name
             image_rating
+            image_rating_count
             audio_rating
+            audio_rating_count
             comfort_rating
+            comfort_rating_count
             bomboniere_rating
+            bomboniere_rating_count
             experience_rating
+            experience_rating_count
             ratings_count
           }
         }
