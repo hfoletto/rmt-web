4da507a542879c6501a9857d994857e763648bf6
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-02-11 18:38:16 -0300

    Add UserRating component

---
+++ components/UserRating.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="py-8 space-y-4">
+    <div class="text-xl">
+      <span class="font-medium">{{ userName }}</span> assistiu
+      <span class="font-bold text-red-800">{{ movieWatched }}</span> em
+      <span class="font-light text-gray-500">{{ visitedAt }}</span>
+    </div>
+    <div class="w-full flex justify-between">
+      <div class="flex items-center">
+        <span class="block">Imagem:</span>
+        <StarRating class="ml-1" :rating="imageRating"/>
+      </div>
+      <div class="flex items-center">
+        <span class="block">Áudio:</span>
+        <StarRating class="ml-1" :rating="audioRating"/>
+      </div>
+      <div class="flex items-center">
+        <span class="block">Conforto:</span>
+        <StarRating class="ml-1" :rating="comfortRating"/>
+      </div>
+      <div class="flex items-center">
+        <span class="block">Bomboniere:</span>
+        <StarRating class="ml-1" :rating="bomboniereRating"/>
+      </div>
+      <div class="flex items-center">
+        <span class="block">Experiência:</span>
+        <StarRating class="ml-1" :rating="experienceRating"/>
+      </div>
+    </div>
+    <div v-if="review" class="text-base leading-7 text-gray-800">
+      <p>{{ review }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  imageRating?: Number,
+  audioRating?: Number,
+  comfortRating?: Number,
+  bomboniereRating?: Number,
+  experienceRating?: Number,
+  review?: Number,
+  movieWatched: Number,
+  visitedAt: Number,
+  userName: String,
+}>()
+</script>
