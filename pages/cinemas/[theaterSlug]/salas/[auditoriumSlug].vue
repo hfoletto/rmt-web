699382db2ae77a86f51cf7f0ab3c95851acfb057
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-02-11 18:38:40 -0300

    Update [auditoriumSlug] page to use UserRating component

--- pages/cinemas/[theaterSlug]/salas/[auditoriumSlug].vue
+++ pages/cinemas/[theaterSlug]/salas/[auditoriumSlug].vue
@@ -1,6 +1,25 @@
 <template>
-  <div v-if="data?.theater?.auditoriums?.length">
-    <h2>{{ data.theater.name }} - {{ data.theater.auditoriums[0].name }}</h2>
+  <div>
+    <div v-if="data?.theater?.auditoriums?.length" class="relative mx-auto max-w-lg lg:max-w-7xl px-6 pt-16 pb-20 lg:px-8 lg:pt-24 lg:pb-28">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        {{ data.theater.auditoriums[0].name }}
+        <span class="ml-2 font-light text-gray-500">{{ data.theater.name }}</span>
+      </h2>
+      <div class="mt-8 divide-y-2 divide-gray-200">
+        <UserRating v-for="(rating, i) in data.theater.auditoriums[0].ratings"
+          :key="i"
+          :image-rating="rating.image_rating"
+          :audio-rating="rating.audio_rating"
+          :comfort-rating="rating.comfort_rating"
+          :bomboniere-rating="rating.bomboniere_rating"
+          :experience-rating="rating.experience_rating"
+          :review="rating.review"
+          :movie-watched="rating.movie_watched"
+          :visited-at="rating.visited_at"
+          :user-name="rating.user.name"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,8 +30,21 @@ const query = gql`
     theater(slug: $theaterSlug) {
       name
       auditoriums(slug: $auditoriumSlug) {
-        name,
+        name
         ratings_count
+        ratings {
+          image_rating
+          audio_rating
+          comfort_rating
+          bomboniere_rating
+          experience_rating
+          review
+          movie_watched
+          visited_at
+          user {
+            name
+          }
+        }
       }
     }
   }
