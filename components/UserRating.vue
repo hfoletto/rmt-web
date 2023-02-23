d890060030bdbd8e4a7cc7ae3e509ef1d15dbd8b
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-02-23 00:28:07 -0300

    Update UserRating component with StarRating new props and date format for visitedAt

--- components/UserRating.vue
+++ components/UserRating.vue
@@ -1,38 +1,37 @@
 <template>
   <div class="py-12 flex">
     <div class="mr-8">
-      <img :src="movieWatchedPoster" class="w-48 h-auto"/>
+      <img :src="movieWatchedPoster" class="w-48 h-auto">
     </div>
     <div class="space-y-4 flex-1">
       <div class="text-xl">
         <span class="font-medium">{{ userName }}</span> assistiu
         <span class="font-bold text-red-800">{{ movieWatchedTitle }}</span> em
-        <span class="font-light text-gray-500">{{ visitedAt }}</span>
+        <span class="font-light tracking-wide text-gray-500">{{ (new Intl.DateTimeFormat('pt-BR')).format((new Date(visitedAt))) }}</span>
       </div>
       <div class="w-full flex justify-between">
         <div class="flex items-center">
           <span class="block">Imagem:</span>
-          <StarRating class="ml-1" :rating="imageRating"/>
+          <StarRating class="ml-1" :model-value="imageRating" readonly />
         </div>
         <div class="flex items-center">
           <span class="block">Áudio:</span>
-          <StarRating class="ml-1" :rating="audioRating"/>
+          <StarRating class="ml-1" :model-value="audioRating" readonly />
         </div>
         <div class="flex items-center">
           <span class="block">Conforto:</span>
-          <StarRating class="ml-1" :rating="comfortRating"/>
+          <StarRating class="ml-1" :model-value="comfortRating" readonly />
         </div>
         <div class="flex items-center">
           <span class="block">Bomboniere:</span>
-          <StarRating class="ml-1" :rating="bomboniereRating"/>
+          <StarRating class="ml-1" :model-value="bomboniereRating" readonly />
         </div>
         <div class="flex items-center">
           <span class="block">Experiência:</span>
-          <StarRating class="ml-1" :rating="experienceRating"/>
+          <StarRating class="ml-1" :model-value="experienceRating" readonly />
         </div>
       </div>
-      <div v-if="review" class="text-base leading-7 text-gray-800 space-y-3" v-html="review">
-      </div>
+      <div v-if="review" class="text-base leading-7 text-gray-800 space-y-3" v-html="review" />
     </div>
   </div>
 </template>
