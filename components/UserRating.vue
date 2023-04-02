e924fa8def248a2a49b2ffdfccc4e652804aff5b
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-04-02 12:07:45 -0300

    Update UserRating component to be responsive

--- components/UserRating.vue
+++ components/UserRating.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="py-12 flex">
-    <div class="mr-8">
-      <img :src="movieWatchedPoster" class="w-48 h-auto">
+  <div class="py-12 md:flex">
+    <div class="hidden md:block w-3/12 mr-2 mb-2 md:w-48 md:mr-8 md:mb-0">
+      <img :src="movieWatchedPoster" class="w-full h-auto">
     </div>
     <div class="space-y-4 flex-1">
-      <div class="text-xl">
-        <span class="font-medium">{{ userName }}</span> assistiu
-        <span class="font-bold text-red-800">{{ movieWatchedTitle }}</span> em
-        <span class="font-light tracking-wide text-gray-500">{{ (new Intl.DateTimeFormat('pt-BR')).format((new Date(visitedAt))) }}</span>
+      <div class="flex items-center max-md:space-y-1 md:block md:text-xl">
+        <div class="block w-3/12 mr-4 md:hidden">
+          <img :src="movieWatchedPoster" class="w-full h-auto">
+        </div>
+        <div>
+          <span class="max-md:block">
+          <span class="font-medium">{{ userName }}</span> assistiu
+        </span>
+          <span class="max-md:block">
+          <span class="font-bold text-red-800">{{ movieWatchedTitle }}</span> em
+        </span>
+          <span class="max-md:block font-light tracking-wide text-gray-500">
+          {{ (new Intl.DateTimeFormat('pt-BR')).format((new Date(visitedAt))) }}
+        </span>
+        </div>
       </div>
-      <div class="w-full flex justify-between">
+      <div class="w-full hidden xl:flex justify-between">
         <div class="flex items-center">
           <span class="block">Imagem:</span>
           <StarRating class="ml-1" :model-value="imageRating" readonly />
@@ -31,6 +42,14 @@
           <StarRating class="ml-1" :model-value="experienceRating" readonly />
         </div>
       </div>
+      <RatingGroup
+        class="w-full justify-center space-x-3.5 xl:hidden"
+        :image-rating="imageRating"
+        :audio-rating="audioRating"
+        :comfort-rating="comfortRating"
+        :bomboniere-rating="bomboniereRating"
+        :experience-rating="experienceRating"
+      />
       <div v-if="review" class="text-base leading-7 text-gray-800 space-y-3" v-html="review" />
     </div>
   </div>
