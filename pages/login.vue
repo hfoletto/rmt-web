57d8488b35d06f9d707b75839d24cef79a50e1d7
Henrique Foletto <henriquevidalfoletto@gmail.com>
2023-04-16 12:13:37 -0300

    Remove remember-me from login page

--- pages/login.vue
+++ pages/login.vue
@@ -33,11 +33,6 @@
               />
 
               <div class="flex items-center justify-between">
-                <div class="flex items-center">
-                  <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-red-800 focus:ring-red-700">
-                  <label for="remember-me" class="ml-2 block text-sm text-gray-900">Lembrar-me</label>
-                </div>
-
                 <div class="text-sm">
                   <a href="#" class="font-medium text-red-800 hover:text-red-700">Esqueceu sua senha?</a>
                 </div>
